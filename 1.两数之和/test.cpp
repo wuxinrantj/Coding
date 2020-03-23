c941be62f4d4b9b82c25e4c8bde9efd6edf7f97f
lenovo <wuxinran@tongji.edu.cn>
2020-03-23 22:21:16 +0800

    .vscode

--- 1.两数之和/test.cpp
+++ 1.两数之和/test.cpp
@@ -9,7 +9,9 @@ public:
     vector<int> static twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> hash;
         for (int i = 0; i < nums.size(); i++) {
-            if (hash.count(target - nums[i])) return { hash[target - nums[i]], i };
+            if (hash.count(target - nums[i])) {
+                return { hash[target - nums[i]], i };
+            }
             hash[nums[i]] = i;
         }
         return { -1, -1 };
