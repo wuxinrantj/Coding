d2c39840d081deaadca15e9a0153cd988d7019f1
lenovo <wuxinran@tongji.edu.cn>
2020-03-26 22:43:36 +0800

    香

--- LeetCode/13.罗马数字转整数/test.cpp
+++ LeetCode/13.罗马数字转整数/test.cpp
@@ -7,13 +7,21 @@ class Solution {
 public:
     int romanToInt(string s) {
         map<char,int> roman;//初始化哈希表
-        roman.insert(map<char,int>::value_type('I',1));
-        roman.insert(map<char,int>::value_type('V',5));
-        roman.insert(map<char,int>::value_type('X',10));
-        roman.insert(map<char,int>::value_type('L',50));
-        roman.insert(map<char,int>::value_type('C',100));
-        roman.insert(map<char,int>::value_type('D',500));
-        roman.insert(map<char,int>::value_type('M',1000));
+        // roman.insert(map<char,int>::value_type('I',1));
+        // roman.insert(map<char,int>::value_type('V',5));
+        // roman.insert(map<char,int>::value_type('X',10));
+        // roman.insert(map<char,int>::value_type('L',50));
+        // roman.insert(map<char,int>::value_type('C',100));
+        // roman.insert(map<char,int>::value_type('D',500));
+        // roman.insert(map<char,int>::value_type('M',1000));
+        roman['I'] = 1;
+        roman['V'] = 5;
+        roman['X'] = 10;
+        roman['L'] = 50;
+        roman['C'] = 100;
+        roman['D'] = 500;
+        roman['M'] = 1000;
+
         int result = 0;
 
         for (int i = 0; i < s.length(); ++i)
