12e309b82baef803fd597821bc11456af22618f3
lenovo <wuxinran@tongji.edu.cn>
2020-03-29 11:13:47 +0800

    solve 27.移除元素

---
+++ LeetCode/27.移除元素/test.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if (nums.empty()){
+            return 0;
+        }else{
+            int j = 0;
+            for (auto it = nums.begin(); it != nums.end();){
+                if (*it == val){
+                    it = nums.erase(it);
+                }else{
+                    it++;
+                    j++;
+                }
+                
+            }
+            for (int k = 0; k < j; ++k){
+                cout<<nums[k]<<' ';
+            }
+            cout<<endl;
+            return j;
+        }
+    }
+};
+
+int main(void)
+{
+    vector<int> nums{3,2,2,3};
+    int val = 3;
+    int ans;
+    Solution so;
+    ans = so.removeElement(nums, val);
+    cout << ans << endl;
+
+    return 0;
+}
