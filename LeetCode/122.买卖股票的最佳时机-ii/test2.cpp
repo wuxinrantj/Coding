244754f12418e4d2b59b2ca54fa9b4552bc2b009
lenovo <wuxinran@tongji.edu.cn>
2020-04-06 12:59:05 +0800

    DP 122

---
+++ LeetCode/122.买卖股票的最佳时机-ii/test2.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+//DP
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int dp_i_0 = 0;
+        int dp_i_1 = INT_MIN;
+
+        for(int i = 0; i < prices.size(); ++i) {
+            int temp = dp_i_0;
+            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = max(dp_i_1, temp - prices[i]);
+        }
+
+        return dp_i_0;
+    }
+};
+
+
+int main(void)
+{
+    vector<int> nums{1,2,3,4,5};
+
+    int ans;
+    Solution so;
+    ans = so.maxProfit(nums);
+    cout << ans << endl;
+
+    return 0;
+}
