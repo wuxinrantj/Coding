51694a2b3739284d8298dc189017fd10e3c76d6e
lenovo <wuxinran@tongji.edu.cn>
2020-04-18 11:22:15 +0800

    solve 51.n皇后

---
+++ LeetCode/51.n皇后/51.n皇后.cpp
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode.cn id=51 lang=cpp
+ *
+ * [51] N皇后
+ *
+ * https://leetcode-cn.com/problems/n-queens/description/
+ *
+ * algorithms
+ * Hard (68.43%)
+ * Likes:    377
+ * Dislikes: 0
+ * Total Accepted:    35.5K
+ * Total Submissions: 51.6K
+ * Testcase Example:  '4'
+ *
+ * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * 
+ * 
+ * 
+ * 上图为 8 皇后问题的一种解法。
+ * 
+ * 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+ * 
+ * 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ * 
+ * 示例:
+ * 
+ * 输入: 4
+ * 输出: [
+ * ⁠[".Q..",  // 解法 1
+ * ⁠ "...Q",
+ * ⁠ "Q...",
+ * ⁠ "..Q."],
+ * 
+ * ⁠["..Q.",  // 解法 2
+ * ⁠ "Q...",
+ * ⁠ "...Q",
+ * ⁠ ".Q.."]
+ * ]
+ * 解释: 4 皇后问题存在两个不同的解法。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<string>> res;
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        backtrack(board, 0);
+        return res;
+    }
+
+    void backtrack(vector<string>& board, int row) {
+        if (row == board.size()) {
+            res.push_back(board);
+            return;
+        }
+        int n = board[row].size();
+        for (int col = 0; col < n; col++) {
+            // 排除不合法选择
+            if (!isValid(board, row, col)) {
+                continue;
+            }
+            // 做选择
+            board[row][col] = 'Q';
+            // 进⼊下⼀⾏决策
+            backtrack(board, row + 1);
+            // 撤销选择
+            board[row][col] = '.';
+        }
+    }
+
+    bool isValid(vector<string>& board, int row, int col) {
+        int n = board.size();
+        // 检查列是否有皇后互相冲突
+        for (int i = 0; i < n; i++) {
+            if (board[i][col] == 'Q')
+            return false;
+        }
+        // 检查右上⽅是否有皇后互相冲突
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+            return false;
+        }
+        // 检查左上⽅是否有皇后互相冲突
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+            return false;
+        }
+        return true;
+    }
+};
+// @lc code=end
+
