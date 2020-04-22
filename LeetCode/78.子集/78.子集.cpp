df5b5e17d4ad96130659efa36512b22e68d7f184
lenovo <wuxinran@tongji.edu.cn>
2020-04-22 10:41:34 +0800

    solve 78.子集

---
+++ LeetCode/78.子集/78.子集.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=cpp
+ *
+ * [78] 子集
+ *
+ * https://leetcode-cn.com/problems/subsets/description/
+ *
+ * algorithms
+ * Medium (76.84%)
+ * Likes:    544
+ * Dislikes: 0
+ * Total Accepted:    81.3K
+ * Total Submissions: 105.5K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * 
+ * 说明：解集不能包含重复的子集。
+ * 
+ * 示例:
+ * 
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ * ⁠ [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // 记录走过的路径
+        vector<int> track;
+        backtrack(nums, 0, track);
+        return res;
+    }
+
+    void backtrack(vector<int>& nums, int start, vector<int>& track) {
+        res.push_back(track);
+        for (int i = start; i < nums.size(); i++) {
+            // 做选择
+            track.push_back(nums[i]);
+            // 回溯
+            backtrack(nums, i + 1, track);
+            // 撤销选择
+            track.pop_back();
+        }
+    }
+};
+// @lc code=end
+
