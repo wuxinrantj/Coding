78bd56ac2501997ef6b43fd86e462b223153e87c
lenovo <wuxinran@tongji.edu.cn>
2020-05-19 11:32:15 +0800

    solve 117.填充每个节点的下一个右侧节点指针-ii

---
+++ LeetCode/117.填充每个节点的下一个右侧节点指针-ii/117.填充每个节点的下一个右侧节点指针-ii.cpp
@@ -0,0 +1,104 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=cpp
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ *
+ * https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/description/
+ *
+ * alg||ithms
+ * Medium (46.16%)
+ * Likes:    131
+ * Dislikes: 0
+ * Total Accepted:    17.8K
+ * Total Submissions: 37.7K
+ * Testcase Example:  '[1,2,3,4,5,null,7]'
+ *
+ * 给定一个二叉树
+ * 
+ * struct Node {
+ * ⁠ int val;
+ * ⁠ Node *left;
+ * ⁠ Node *right;
+ * ⁠ Node *next;
+ * }
+ * 
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ * 
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 
+ * 输入：root = [1,2,3,4,5,null,7]
+ * 输出：[1,#,2,3,#,4,5,7,#]
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中的节点数小于 6000
+ * -100 <= node.val <= 100
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition f|| a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root && (root->left || root->right)){
+            if(root->left && root->right) root->left->next = root->right;
+            // 有一个子节点：将这个子节点的 next 属性设置为同层的下一个节点，
+            // 即为 root.next 的最左边的一个节点，如果 root.next 没有子节点，则考虑 root.next.next，依次类推
+            Node *node = root->right ? root->right : root->left;
+            Node *head = root->next;
+            while (head && !(head->left || head->right)){
+                head = head->next;
+            }
+            node->next = head ? (head->left ? head->left : head->right) : nullptr;
+            
+            connect(root->right);
+            connect(root->left);
+        }
+        return root;
+    }
+};
+// @lc code=end
+
